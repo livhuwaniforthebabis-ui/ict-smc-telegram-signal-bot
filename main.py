d10709ac1dc282c203a1088b4790742ac21cb856
livhucart <livhuwaniforthebabis@gmail.com>
2026-04-02 18:16:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import schedule
 import time
 
-from strategy.smc_strategy import scan_markets
+from strategy.ict_smc_strategy import scan_markets
 from bot.telegram_bot import send_analysis, send_signal
 from tracker.trade_tracker import TradeTracker
 from config import MAX_TRADES_PER_DAY
@@ -9,24 +9,27 @@ from config import MAX_TRADES_PER_DAY
 tracker = TradeTracker()
 
 
-def run_bot():
+def run_signal_scan():
 
     if tracker.trades_today >= MAX_TRADES_PER_DAY:
         return
 
     setup = scan_markets()
 
-    if not setup:
+    if setup is None:
         return
 
     send_analysis(setup)
+    time.sleep(5)
 
     send_signal(setup)
 
     tracker.record_trade(setup)
 
 
-schedule.every(5).minutes.do(run_bot)
+schedule.every(5).minutes.do(run_signal_scan)
+
+print("BOT RUNNING...")
 
 while True:
     schedule.run_pending()
